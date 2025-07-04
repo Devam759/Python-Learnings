1b197dd1d8f1e5bfaa0e0e86ab8495ba43daf5c2
Devam Gupta <devamg759@gmail.com>
2025-07-05 02:36:28 +0530

    mm

--- 1.py
+++ 1.py
@@ -8,12 +8,27 @@ rows = 3
 cols = 3
 
 symbol_count = {
-    "A": 2,
+    "A": 5,
     "B": 4,
-    "C": 6,
-    "D": 8,
+    "C": 3,
+    "D": 2,
 }
 
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            
+    return winnings
+                
+
+    
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -35,15 +50,13 @@ def get_slot_machine_spin(rows, cols, symbols):
 
 def print_slot_machine(columns):
     for row in range(len(columns[0])):
-        for i,column in enumerate(columns):
-            if i!= len(columns)-1:
-                print(column[row],end = " | ")
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end = " | ")
             else:
                 print(column[row], end = "")
-                
-        print()
-                
-    
+                print()
+                                
     
 def deposit():
     while True:
